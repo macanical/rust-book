962d79027803ce88f603128e24964acf1f28387b
Jordan McCann <jordanmccann1st@outlook.com>
2021-05-16 22:53:13 -0500

    Finished chapter 3.2

---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/home/pi/Projects/rust-book/data_types/target/debug/data_types: /home/pi/Projects/rust-book/data_types/src/main.rs
---
+++ data_types/target/debug/deps/data_types-9be11c638668b82a.d
@@ -0,0 +1,5 @@
+/home/pi/Projects/rust-book/data_types/target/debug/deps/data_types-9be11c638668b82a: src/main.rs
+
+/home/pi/Projects/rust-book/data_types/target/debug/deps/data_types-9be11c638668b82a.d: src/main.rs
+
+src/main.rs:
